202699fda07777d6baf0d166f7ccbc5953f7e11e
jenativi <jenativi@hawaii.edu>
2024-03-07 17:58:01 -1000

    simple schema

--- app/imports/api/PeopleCollection.js
+++ app/imports/api/PeopleCollection.js
@@ -1,3 +1,16 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import 'meteor/aldeed:collection2/static';
+
+const peopleSchema = new SimpleSchema(
+  {
+    first: String,
+    last: String,
+    age: SimpleSchema.Integer,
+    city: String
+  }
+);
 
 export const PeopleCollection = new Mongo.Collection('People');
+
+PeopleCollection.attachSchema(peopleSchema);
